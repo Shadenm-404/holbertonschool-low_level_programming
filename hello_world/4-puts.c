e83297dc41530da92b7a042931fb061bce177d37
Shadenm-404 <viuxwl7@gmail.com>
2025-08-25 08:41:22 +0000

    Fix Betty: use tabs for indentation in 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,3 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
-}
+:set noexpandtab
+:10,11s/^\s\+/\t/
+:wq
