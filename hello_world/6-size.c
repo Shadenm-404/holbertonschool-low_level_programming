0ac5785120d2927ee0de4aeabe4ff3616935205e
Shadenm-404 <viuxwl7@gmail.com>
2025-08-25 08:59:01 +0000

    Task 6: fix Betty (tabs, wrap lines <80) and print sizes

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Print sizes of various C types
+ * main - Prints the size of various C types
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
-    printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n",
+		(unsigned long)sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n",
+		(unsigned long)sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 	return (0);
 }
