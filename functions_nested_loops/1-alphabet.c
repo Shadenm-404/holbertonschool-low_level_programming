62397ae47872b62c4a76fe38bd8a846ab8944662
Shadenm-404 <viuxwl7@gmail.com>
2025-08-27 06:35:07 +0000

    functions_nested_loops: add 1-alphabet.c and update main.h

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+	* print_alphabet - prints the lowercase alphabet followed by a new line
+	*/
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	_putchar(ch);
+
+	_putchar('\n');
+}
