94dcd89536ca822af4cab84bece0f67bcbd3b0fa
Shadenm-404 <viuxwl7@gmail.com>
2025-08-27 06:20:43 +0000

    Fix: add Betty doc and formatting for 101-print_comb4.c

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+	* main - Prints all different combinations of three digits
+	*
+	* Return: Always 0 (Success)
+	*/
 int main(void)
 {
 	int i, j, k;
+
 
 	for (i = 0; i <= 7; i++)
 	{
