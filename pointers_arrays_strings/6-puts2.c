991dc6f1bd280e551940a7137e1b86553cd56b5b
Shadenm-404 <viuxwl7@gmail.com>
2025-09-05 09:20:37 +0000

    Task 6: puts2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,27 +1,14 @@
 #include "main.h"
-
-/**
- * puts2 - prints every other character of a string, starting with the first
- *         followed by a new line
- * @str: pointer to the string
- *
- * Description: Prints indices 0, 2, 4, ... until '\0', then prints '\n'.
- */
 void puts2(char *str)
 {
 	int i;
-
 	if (str == 0)
 	{
-		_putchar('\n');
+		_putchar(n);
 		return;
 	}
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
+	for (i = 0; str[i] != 0; i++)
 		if ((i % 2) == 0)
 			_putchar(str[i]);
-	}
-
-	_putchar('\n');
+	_putchar(n);
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,7 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 int _putchar(char c);
 void puts2(char *str);
-
-#endif /* MAIN_H */
+#endif
