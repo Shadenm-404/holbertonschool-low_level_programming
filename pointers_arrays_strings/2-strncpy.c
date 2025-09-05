ddb70f635d7e775b3a51bf255aa9caa5d5222412
Shadenm-404 <viuxwl7@gmail.com>
2025-09-05 11:13:18 +0000

    Fix: remove forbidden stdlib token from comments in 2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * _strncpy - copies a string into dest using at most n bytes
+ * _strncpy - copy a string into dest using at most n bytes
  * @dest: destination buffer
  * @src: source string
  * @n: maximum number of bytes to copy
  *
- * Description: Behaves like strncpy:
- * - Copy up to n bytes from @src to @dest.
- * - If @src is shorter than n, pad the rest with '\0'.
- * - If @src has n or more bytes, no extra '\0' is appended.
+ * Description: Copies up to @n bytes from @src to @dest.
+ * If @src is shorter than @n, the remainder is padded with '\0'.
+ * If @src has @n or more bytes, no extra '\0' is appended.
  * Return: pointer to dest
  */
 char *_strncpy(char *dest, char *src, int n)
