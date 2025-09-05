3b67c80f29b34a0586d4691209538e5925f6c073
Shadenm-404 <viuxwl7@gmail.com>
2025-09-05 10:41:00 +0000

    Solve: pointers_arrays_strings/9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string from src to dest (including '\0')
+ * @dest: destination buffer
+ * @src: source string
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	if (!dest || !src)
+		return (dest);
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
