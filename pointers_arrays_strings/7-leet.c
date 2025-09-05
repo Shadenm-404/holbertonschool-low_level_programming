5b726c6ed67be06be4c38d1afac8efe9dd573ec4
Shadenm-404 <viuxwl7@gmail.com>
2025-09-05 11:31:50 +0000

    Fix: 7-leet with 0 ifs, 2 loops via lookup table

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -4,29 +4,37 @@
  * leet - encodes a string into 1337
  * @s: pointer to the string to modify
  *
- * Mapping:
- * a/A->4, e/E->3, o/O->0, t/T->7, l/L->1
+ * Maps: a/A->4, e/E->3, o/O->0, t/T->7, l/L->1.
+ * Two loops total: table init + walk over the string.
  * Return: pointer to the modified string
  */
 char *leet(char *s)
 {
-	int i = 0, j;
-	char *letters = "aAeEoOtTlL";
-	char *codes =   "4433007711";
+	int i = 0, k;
+	unsigned char uc;
+	char map[256];
 
-	if (!s)
-		return (s);
+	/* init lookup table to identity */
+	for (k = 0; k < 256; k++)
+		map[k] = (char)k;
 
-	while (s[i] != '\0')
+	/* set leet mappings */
+	map[(unsigned char) 'a'] = '4';
+	map[(unsigned char) 'A'] = '4';
+	map[(unsigned char) 'e'] = '3';
+	map[(unsigned char) 'E'] = '3';
+	map[(unsigned char) 'o'] = '0';
+	map[(unsigned char) 'O'] = '0';
+	map[(unsigned char) 't'] = '7';
+	map[(unsigned char) 'T'] = '7';
+	map[(unsigned char) 'l'] = '1';
+	map[(unsigned char) 'L'] = '1';
+
+	/* walk the string; no if needed */
+	while (s && s[i] != '\0')
 	{
-		for (j = 0; letters[j] != '\0'; j++)
-		{
-			if (s[i] == letters[j])
-			{
-				s[i] = codes[j];
-				break;
-			}
-		}
+		uc = (unsigned char) s[i];
+		s[i] = map[uc];
 		i++;
 	}
 	return (s);
