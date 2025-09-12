276533924b874f86b071d7797b832cd62f91fb86
Shadenm-404 <viuxwl7@gmail.com>
2025-09-12 09:32:39 +0000

    fix: add 1-memcpy.c in correct path for task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,15 +1,21 @@
 #include "main.h"
+
 /**
- * _memcpy - ينسخ n بايت من src إلى dest (بدون دعم التداخل)
- * @dest: وجهة النسخ
- * @src: مصدر النسخ
- * @n: عدد البايتات
- * Return: dest
+ * _memcpy - copy n bytes from src to dest (no support for overlap)
+ * @dest: destination buffer
+ * @src:  source buffer
+ * @n:    number of bytes to copy
+ *
+ * Return: pointer to dest
+ *
+ * Note: behavior is undefined if memory regions overlap (like standard memcpy).
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
     unsigned int i;
+
     for (i = 0; i < n; i++)
         dest[i] = src[i];
+
     return dest;
 }
