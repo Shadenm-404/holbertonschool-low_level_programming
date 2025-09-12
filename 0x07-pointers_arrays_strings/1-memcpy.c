adcd91cbed3c128fbd3525fef14a65d0ac3e24d1
Shadenm-404 <viuxwl7@gmail.com>
2025-09-12 09:35:41 +0000

    fix: add 1-memcpy.c in correct path (0x07-pointers_arrays_strings)

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,21 +1,16 @@
 #include "main.h"
 
 /**
- * _memcpy - copy n bytes from src to dest (no support for overlap)
- * @dest: destination buffer
- * @src:  source buffer
- * @n:    number of bytes to copy
- *
- * Return: pointer to dest
- *
- * Note: behavior is undefined if memory regions overlap (like standard memcpy).
+ * _memcpy - copy n bytes from src to dest (no overlap support)
+ * @dest: destination
+ * @src:  source
+ * @n:    number of bytes
+ * Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
     unsigned int i;
-
     for (i = 0; i < n; i++)
         dest[i] = src[i];
-
     return dest;
 }
