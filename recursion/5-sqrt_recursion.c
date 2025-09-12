1dd6b3ddda3445274aa4603caeb3d244f9bb7506
Shadenm-404 <viuxwl7@gmail.com>
2025-09-12 10:21:43 +0000

    recursion: fix Betty doc for _sqrt_recursion and helper; no static

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,7 +1,13 @@
 #include "main.h"
 
-int _sqrt_helper(int n, int g);
-
+/**
+ * _sqrt_helper - helper to find a natural square root recursively
+ * @n: the number to get the square root of
+ * @g: current guess to test (starting from 1 and increasing)
+ *
+ * Return: the natural square root if found,
+ *         -1 if no natural square root exists for @n
+ */
 int _sqrt_helper(int n, int g)
 {
 	if (g > n / g)
@@ -11,6 +17,13 @@ int _sqrt_helper(int n, int g)
 	return (_sqrt_helper(n, g + 1));
 }
 
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: input integer
+ *
+ * Return: the natural square root of @n,
+ *         or -1 if @n is negative or has no natural square root
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
