1d1b090799b267a85267867c5e9f7013714f813c
Shadenm-404 <viuxwl7@gmail.com>
2025-09-19 17:58:23 +0000

    Betty: fix indent (tabs) for 3-array_range.c at repo root

---
+++ 3-array_range.c
@@ -0,0 +1,24 @@
+#include "main.h"
+	/**
+	* array_range - creates an array of ints from min to max (inclusive)
+	* @min: starting value (included)
+	* @max: ending value (included)
+	*
+	* Return: pointer to newly allocated array ordered from min to max,
+	*         or NULL if min > max or if allocation fails.
+	*/
+int *array_range(int min, int max)
+{
+	unsigned int len, i;
+	int *arr;
+	if (min > max)
+	return (NULL);
+	/* C90-safe: بدون long long / SIZE_MAX */
+	len = (unsigned int)(max - min + 1);
+	arr = (int *)malloc(len * sizeof(int));
+	if (arr == NULL)
+	return (NULL);
+	for (i = 0; i < len; i++)
+	arr[i] = min + (int)i;
+	return (arr);
+}
