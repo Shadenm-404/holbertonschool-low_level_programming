b74ad252e1555577899b2ca0c2ea0ce52c78fc1a
Shadenm-404 <viuxwl7@gmail.com>
2025-09-19 18:10:55 +0000

    structures_typedef: add dog.h (struct dog) and 1-init_dog.c (init_dog)

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - basic dog structure
+ * @name: pointer to dog's name
+ * @age: dog's age
+ * @owner: pointer to owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/* Task 1 prototype */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif /* DOG_H */
