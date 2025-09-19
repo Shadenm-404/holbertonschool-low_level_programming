d893059f5dbd43014b21a34d3565422e0e93d9d1
Shadenm-404 <viuxwl7@gmail.com>
2025-09-19 17:30:32 +0000

    more_malloc_free: implement 2-calloc with overflow guard and manual zeroing

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,45 +1,35 @@
 #include "main.h"
 
 /**
- * _memzero - set n bytes of s to 0
- * @s: pointer to memory
- * @n: bytes to zero
- */
-static void _memzero(unsigned char *s, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		s[i] = 0;
-}
-
-/**
  * _calloc - allocates memory for an array and sets it to zero
  * @nmemb: number of elements
- * @size: size of each element
+ * @size: size of each element in bytes
  *
  * Return: pointer to allocated zeroed memory, or NULL on failure
  * Notes:
- * - If nmemb or size is 0 -> return NULL (Holberton requirement)
- * - Prevent overflow on nmemb * size
+ * - If nmemb == 0 or size == 0 -> return NULL
+ * - If nmemb * size overflows -> return NULL
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
-	unsigned int total;
+	unsigned int total, i;
+	unsigned char *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	/* overflow guard: nmemb * size must fit in unsigned int */
+	/* overflow guard */
 	if (nmemb > UINT_MAX / size)
 		return (NULL);
 
 	total = nmemb * size;
-	p = malloc(total);
+
+	p = (unsigned char *)malloc(total);
 	if (p == NULL)
 		return (NULL);
 
-	_memzero((unsigned char *)p, total);
-	return (p);
+	for (i = 0; i < total; i++)
+		p[i] = 0;
+
+	return ((void *)p);
 }
