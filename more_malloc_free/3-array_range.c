3bf2985ea0982cd41238fdd1237e581152b54f55
Shadenm-404 <viuxwl7@gmail.com>
2025-09-19 17:38:56 +0000

    imore_malloc_free: implement 3-array_range (Betty-compliant C90 safe)

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -10,25 +10,21 @@
  */
 int *array_range(int min, int max)
 {
-	unsigned int len, i;
-	int *arr;
+    unsigned int len, i;
+    int *arr;
 
-	if (min > max)
-		return (NULL);
+    if (min > max)
+        return (NULL);
 
-	/* length = max - min + 1 (avoid UB with wider arithmetic) */
-	len = (unsigned int)((long long)max - (long long)min + 1);
+    /* C90-safe: بدون long long / SIZE_MAX */
+    len = (unsigned int)(max - min + 1);
 
-	/* guard against overflow in len * sizeof(int) */
-	if ((size_t)len > SIZE_MAX / sizeof(int))
-		return (NULL);
+    arr = (int *)malloc(len * sizeof(int));
+    if (arr == NULL)
+        return (NULL);
 
-	arr = (int *)malloc(len * sizeof(int));
-	if (arr == NULL)
-		return (NULL);
+    for (i = 0; i < len; i++)
+        arr[i] = min + (int)i;
 
-	for (i = 0; i < len; i++)
-		arr[i] = min + (int)i;
-
-	return (arr);
+    return (arr);
 }
