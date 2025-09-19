70eb32341b447d3cb1383b965f41b3c05a27bd1c
Shadenm-404 <viuxwl7@gmail.com>
2025-09-19 18:19:32 +0000

    structures_typedef: fix dog.h include guard; add 2-print_dog.c (Betty-compliant, tabs); place files at root + dir

--- 2-print_dog.c
+++ 2-print_dog.c
@@ -2,12 +2,12 @@
 #include "dog.h"
 
 /**
- * print_dog - prints a struct dog
- * @d: pointer to struct dog
- *
- * Description: If an element is NULL, prints "(nil)" for it.
- * If d is NULL, prints nothing.
- */
+	* print_dog - prints a struct dog
+	* @d: pointer to struct dog
+	*
+	* Description: If an element is NULL, prints "(nil)" for it.
+	* If d is NULL, prints nothing.
+	*/
 void print_dog(struct dog *d)
 {
 	if (d == 0)
