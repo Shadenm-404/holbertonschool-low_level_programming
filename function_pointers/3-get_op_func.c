e1da5c6e254ed497e847ac66625244870219055e
Shadenm-404 <viuxwl7@gmail.com>
2025-09-26 15:44:05 +0000

    feat(function_pointers): add task 3 (Betty-compliant)

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,6 +1,28 @@
 #include "3-calc.h"
-int (*get_op_func(char *s))(int,int){
-	op_t ops[]={{"+",op_add},{"-",op_sub},{"*",op_mul},{"/",op_div},{"%",op_mod},{0,0}};
-	int i=0; while(ops[i].op){ if(*ops[i].op==*s && s[1]=='\0') return ops[i].f; i++; }
-	return 0;
+
+/**
+ * get_op_func - selects the correct function for an operator
+ * @s: operator string
+ *
+ * Return: pointer to function, or NULL if not found
+ *
+ * Notes:
+ * - only one while and one if here
+ * - no switch, for, do, goto, or else
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add}, {"-", op_sub}, {"*", op_mul},
+		{"/", op_div}, {"%", op_mod}, {0, 0}
+	};
+	int i = 0;
+
+	while (ops[i].op)
+	{
+		if (*ops[i].op == *s && s[1] == '\0')
+			return (ops[i].f);
+		i++;
+	}
+	return (0);
 }
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,11 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
-int main(int argc,char *argv[]){
-	int a,b,res; int (*f)(int,int);
-	if(argc!=4){printf("Error\n");exit(98);}
-	f=get_op_func(argv[2]);
-	if(f==0||argv[2][1]!='\0'){printf("Error\n");exit(99);}
-	if((argv[2][0]=='/'||argv[2][0]=='%')&&atoi(argv[3])==0){printf("Error\n");exit(100);}
-	a=atoi(argv[1]); b=atoi(argv[3]); res=f(a,b); printf("%d\n",res); return 0;
+
+/**
+ * main - performs simple operations
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success; exits 98/99/100 on errors
+ *
+ * Constraints: max 3 if statements; no loops here.
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, res;
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	f = get_op_func(argv[2]);
+	if (f == 0 || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') &&
+	    atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	res = f(a, b);
+	printf("%d\n", res);
+
+	return (0);
 }
