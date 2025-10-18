e8a4856d237d9029457df24e1591e7f2f57634a2
Shaden Alalwani <viuxwl7@gmail.com>
2025-10-18 18:58:07 +0000

    Fix: handle read() errors correctly; return 98 when read fails

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,15 +1,23 @@
+/* 3-cp.c */
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
 
-char *create_buffer(char *file);
-void close_file(int fd);
+#define BUF_SIZE 1024
 
-/* Alloc 1024 bytes (exit 99 on fail) */
-char *create_buffer(char *file)
+static char *create_buffer(const char *file);
+static void close_file(int fd);
+
+/**
+ * create_buffer - allocate a 1KB buffer.
+ * @file: name of destination file (for error message).
+ * Return: pointer to allocated buffer.
+ */
+static char *create_buffer(const char *file)
 {
-	char *buffer = malloc(1024);
+	char *buffer = malloc(BUF_SIZE);
 
 	if (buffer == NULL)
 	{
@@ -19,8 +27,11 @@ char *create_buffer(char *file)
 	return (buffer);
 }
 
-/* Safe close (exit 100 on fail) */
-void close_file(int fd)
+/**
+ * close_file - close fd and handle error.
+ * @fd: file descriptor to close.
+ */
+static void close_file(int fd)
 {
 	if (close(fd) == -1)
 	{
@@ -29,86 +40,80 @@ void close_file(int fd)
 	}
 }
 
-/*
- * cp: exit codes
- * 97 - wrong args
- * 98 - read error (file_from)
- * 99 - write/create error (file_to)
- * 100 - close error
+/**
+ * main - copy content of a file to another.
+ * @argc: argument count.
+ * @argv: argument vector.
+ * Return: 0 on success.
+ *
+ * Exit codes:
+ * 97 - wrong usage,
+ * 98 - read error,
+ * 99 - write/create error,
+ * 100 - close error.
  */
 int main(int argc, char *argv[])
 {
-	int from, to;
-	ssize_t r, w, written;
+	int fd_from, fd_to;
+	ssize_t r, w;
 	char *buffer;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		return (97);
+		exit(97);
 	}
 
 	buffer = create_buffer(argv[2]);
 
-	from = open(argv[1], O_RDONLY);
-	if (from == -1)
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		free(buffer);
-		return (98);
+		exit(98);
 	}
 
-	/* اقرأ أولاً. لو فشلت القراءة ← 98 بدون فتح file_to */
-	r = read(from, buffer, 1024);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		free(buffer);
+		close_file(fd_from);
+		exit(99);
+	}
+
+	while ((r = read(fd_from, buffer, BUF_SIZE)) > 0)
+	{
+		ssize_t total = 0;
+
+		while (total < r)
+		{
+			w = write(fd_to, buffer + total, r - total);
+			if (w == -1)
+			{
+				dprintf(STDERR_FILENO,
+					"Error: Can't write to %s\n", argv[2]);
+				free(buffer);
+				close_file(fd_from);
+				close_file(fd_to);
+				exit(99);
+			}
+			total += w;
+		}
+	}
+
 	if (r == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		free(buffer);
-		close_file(from);
-		return (98);
-	}
-
-	/* افتح file_to فقط بعد نجاح أول read */
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		free(buffer);
-		close_file(from);
-		return (99);
-	}
-
-	/* اكتب الـ chunk الأول، ثم أكمل حلقات read/write */
-	while (r > 0)
-	{
-		written = 0;
-		while (written < r)
-		{
-			w = write(to, buffer + written, r - written);
-			if (w == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-				free(buffer);
-				close_file(from);
-				close_file(to);
-				return (99);
-			}
-			written += w;
-		}
-
-		r = read(from, buffer, 1024);
-		if (r == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			free(buffer);
-			close_file(from);
-			close_file(to);
-			return (98);
-		}
+		close_file(fd_from);
+		close_file(fd_to);
+		exit(98);
 	}
 
 	free(buffer);
-	close_file(from);
-	close_file(to);
+	close_file(fd_from);
+	close_file(fd_to);
 	return (0);
 }
