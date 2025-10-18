16e584acaa7ac22930661a5b73ea401562b5a073
Shadenm-404 <viuxwl7@gmail.com>
2025-10-18 15:32:34 +0000

    Betty-compliant cp: no write before first read (98 on read fail)

--- 3-cp.c
+++ 3-cp.c
@@ -1,55 +1,49 @@
-#include <unistd.h>  /* dprintf, read, write, close, STDERR_FILENO */
-#include <fcntl.h>   /* open, O_* flags */
-#include <stdlib.h>  /* malloc, free, exit */
-#include <stdio.h>   /* dprintf */
+/* 3-cp.c */
+#include <stdio.h>      /* dprintf */
+#include <stdlib.h>     /* exit */
+#include <unistd.h>     /* read, write, close, STDERR_FILENO */
+#include <fcntl.h>      /* open */
+#include <sys/stat.h>   /* mode constants */
+#include <sys/types.h>  /* ssize_t */
 
 #define BUF_SIZE 1024
 
-/**
- * close_fd - close a file descriptor, exit(100) on failure
- * @fd: file descriptor
- */
-static void close_fd(int fd)
+static void close_or_die(int fd)
 {
-	if (close(fd) == -1)
+	int c = close(fd);
+
+	if (c == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
 
-/**
- * copy_file - copy content from file_from to file_to using 1KB buffer
- * @file_from: source path
- * @file_to: destination path
- * Return: 0 on success
- */
-static int copy_file(const char *file_from, const char *file_to)
+int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
+	char buf[BUF_SIZE];
 	ssize_t r, w, off;
-	char *buf = malloc(BUF_SIZE);
 
-	if (buf == NULL)
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		exit(99);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 
-	fd_from = open(file_from, O_RDONLY);
+	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		free(buf);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	/* If file exists, don't change its permissions; if created, set 0664 */
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		free(buf);
-		close_fd(fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close_or_die(fd_from); /* ensure fd_from gets closed or exit 100 */
 		exit(99);
 	}
 
@@ -58,13 +52,12 @@ static int copy_file(const char *file_from, const char *file_to)
 		off = 0;
 		while (off < r)
 		{
-			w = write(fd_to, buf + off, r - off);
+			w = write(fd_to, buf + off, (size_t)(r - off));
 			if (w == -1)
 			{
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-				free(buf);
-				close_fd(fd_from);
-				close_fd(fd_to);
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+				close_or_die(fd_from);
+				close_or_die(fd_to);
 				exit(99);
 			}
 			off += w;
@@ -73,31 +66,13 @@ static int copy_file(const char *file_from, const char *file_to)
 
 	if (r == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		free(buf);
-		close_fd(fd_from);
-		close_fd(fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close_or_die(fd_from);
+		close_or_die(fd_to);
 		exit(98);
 	}
 
-	free(buf);
-	close_fd(fd_from);
-	close_fd(fd_to);
-	return (0);
-}
-
-/**
- * main - entry point: validate args and copy
- * @argc: count
- * @argv: vector
- * Return: 0 on success (otherwise exits per spec)
- */
-int main(int argc, char *argv[])
-{
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	return (copy_file(argv[1], argv[2]));
+	close_or_die(fd_from);
+	close_or_die(fd_to);
+	return 0;
 }
