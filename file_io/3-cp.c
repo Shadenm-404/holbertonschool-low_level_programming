657ad701d09f29318e534afbc1aa773997735e1c
Shadenm-404 <viuxwl7@gmail.com>
2025-10-18 15:17:15 +0000

    file_io: ensure 3-cp.c checks read() before opening dest (exit 98 on read failure)

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,12 +1,21 @@
-#include <stdio.h>      /* dprintf */
-#include <stdlib.h>     /* exit */
-#include <unistd.h>     /* read, write, close */
-#include <fcntl.h>      /* open */
-#include <sys/stat.h>   /* modes */
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-/* ===== Helpers (4 funcs total including main) ===== */
+/**
+ * print_usage_and_exit - prints usage and exits with code 97
+ */
+static void print_usage_and_exit(void)
+{
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
+}
 
-static void close_fail(int fd)
+/**
+ * close_or_die - closes fd or exits with 100 on failure
+ * @fd: file descriptor to close
+ */
+static void close_or_die(int fd)
 {
 	if (close(fd) == -1)
 	{
@@ -15,85 +24,94 @@ static void close_fail(int fd)
 	}
 }
 
-static void write_all(int fd_to, char *buf, ssize_t n,
-		      const char *file_to, int fd_from)
+/**
+ * write_fully - writes exactly n bytes; exits 99 on failure
+ * @fd: destination fd
+ * @buf: buffer
+ * @n: number of bytes
+ * @to_name: dest file name (for message)
+ */
+static void write_fully(int fd, const char *buf, ssize_t n, const char *to_name)
 {
 	ssize_t off = 0, w;
 
 	while (off < n)
 	{
-		w = write(fd_to, buf + off, n - off);
+		w = write(fd, buf + off, n - off);
 		if (w == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-			close_fail(fd_from);
-			close_fail(fd_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to_name);
 			exit(99);
 		}
 		off += w;
 	}
 }
 
-static void copy_file(const char *src, const char *dst)
+/**
+ * main - copies file_from to file_to (POSIX I/O only)
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
 	ssize_t r;
 	char buf[1024];
 
-	fd_from = open(src, O_RDONLY);
+	if (argc != 3)
+		print_usage_and_exit();
+
+	/* افتح المصدر */
+	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	/* أول read: لو فشلت نخرج 98 بدون فتح الوجهة */
-	r = read(fd_from, buf, sizeof(buf));
+	/* جرّب القراءة أولاً قبل فتح الوجهة لضمان 98 عند فشل read */
+	r = read(fd_from, buf, 1024);
 	if (r == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
-		close_fail(fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close_or_die(fd_from);
 		exit(98);
 	}
 
-	fd_to = open(dst, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	/* افتح/أنشئ الوجهة الآن */
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dst);
-		close_fail(fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close_or_die(fd_from);
 		exit(99);
 	}
 
-	while (r > 0)
+	/* اكتب أول كتلة لو كانت القراءة نجحت */
+	if (r > 0)
+		write_fully(fd_to, buf, r, argv[2]);
+
+	/* كمّل النسخ */
+	while (1)
 	{
-		write_all(fd_to, buf, r, dst, fd_from);
-		r = read(fd_from, buf, sizeof(buf));
+		r = read(fd_from, buf, 1024);
 		if (r == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
-			close_fail(fd_from);
-			close_fail(fd_to);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			close_or_die(fd_from);
+			close_or_die(fd_to);
 			exit(98);
 		}
+		if (r == 0)
+			break;
+
+		write_fully(fd_to, buf, r, argv[2]);
 	}
 
-	close_fail(fd_from);
-	close_fail(fd_to);
-}
-
-/**
- * main - copy content of a file to another file
- * @ac: argument count
- * @av: argument vector
- * Return: 0 on success; exits 97/98/99/100 on error
- */
-int main(int ac, char **av)
-{
-	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	copy_file(av[1], av[2]);
+	close_or_die(fd_from);
+	close_or_die(fd_to);
 	return (0);
 }
+
