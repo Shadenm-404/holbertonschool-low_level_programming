481db3a9861858195c29b232218de3525de49490
Shadenm-404 <viuxwl7@gmail.com>
2025-10-18 15:04:30 +0000

    file_io: refactor 3-cp.c to satisfy Betty (split into small helpers)

--- 3-cp.c
+++ 3-cp.c
@@ -1,13 +1,17 @@
-#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define BUF_SIZE 1024
 
 /**
- * create_buffer - Allocate a 1KB buffer for file I/O.
- * @file: Name of the output file (for error message context).
- *
- * Return: Pointer to the allocated buffer.
- * Exit: 99 if allocation fails.
+ * create_buffer - allocate a 1KB buffer and handle allocation error.
+ * @file: file name used in the error message context.
+ * Return: pointer to buffer.
+ * Exit: 99 on allocation failure.
  */
-char *create_buffer(const char *file)
+static char *create_buffer(const char *file)
 {
 	char *buffer = malloc(BUF_SIZE);
 
@@ -20,12 +24,10 @@ char *create_buffer(const char *file)
 }
 
 /**
- * close_fd - Close a file descriptor with error check.
- * @fd: The file descriptor to close.
- *
- * Exit: 100 if close(2) fails.
+ * close_fd - close a file descriptor, exiting with code 100 on failure.
+ * @fd: file descriptor to close.
  */
-void close_fd(int fd)
+static void close_fd(int fd)
 {
 	if (close(fd) == -1)
 	{
@@ -35,28 +37,43 @@ void close_fd(int fd)
 }
 
 /**
- * cp - Copy content from one file to another using a 1KB buffer.
- * @file_from: Source file path.
- * @file_to: Destination file path.
+ * main - copy the content of a file to another file.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Usage: cp file_from file_to
+ * Exit codes:
+ * 97: wrong number of args
+ * 98: read error on file_from
+ * 99: write/create error on file_to or buffer allocation error
+ * 100: close error
  *
  * Return: 0 on success.
- * Exit: 98 on read errors, 99 on write/create errors.
  */
-int cp(const char *file_from, const char *file_to)
+int main(int argc, char **argv)
 {
 	int fd_from, fd_to, r, w;
-	char *buffer = create_buffer(file_to);
+	char *buffer;
 
-	fd_from = open(file_from, O_RDONLY);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer = create_buffer(argv[2]);
+
+	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		free(buffer), exit(98);
 	}
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		free(buffer), close_fd(fd_from), exit(99);
 	}
 
@@ -65,13 +82,14 @@ int cp(const char *file_from, const char *file_to)
 		w = write(fd_to, buffer, r);
 		if (w != r)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			free(buffer), close_fd(fd_from), close_fd(fd_to), exit(99);
 		}
 	}
+
 	if (r == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		free(buffer), close_fd(fd_from), close_fd(fd_to), exit(98);
 	}
 
@@ -79,22 +97,4 @@ int cp(const char *file_from, const char *file_to)
 	close_fd(fd_from);
 	close_fd(fd_to);
 	return (0);
-}
-
-/**
- * main - Entry point: validate args and copy a file.
- * @argc: Argument count.
- * @argv: Argument vector.
- *
- * Return: 0 on success.
- * Exit: 97 if usage is wrong.
- */
-int main(int argc, char **argv)
-{
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	return (cp(argv[1], argv[2]));
 }
