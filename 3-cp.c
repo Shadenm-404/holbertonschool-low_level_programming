6e7d89d205329529450e0ff49fb1fc143fe4bcdb
Shadenm-404 <viuxwl7@gmail.com>
2025-10-18 15:19:13 +0000

    Betty-compliant cp: no write before first read (98 on read fail)

--- 3-cp.c
+++ 3-cp.c
@@ -1,15 +1,14 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #define BUF_SIZE 1024
 
-/**
- * closing - close a file descriptor or exit(100) on failure
- * @fd: file descriptor to close
- */
-static void closing(int fd)
+/* ---------- helpers ---------- */
+
+static void close_fail(int fd)
 {
 	if (close(fd) == -1)
 	{
@@ -18,74 +17,95 @@ static void closing(int fd)
 	}
 }
 
-/**
- * copy_file - copy content from file_from to file_to using 1KB buffer
- * @file_from: source path
- * @file_to: destination path
- * Return: 0 on success
- */
-static int copy_file(const char *file_from, const char *file_to)
+static void write_all(int fd_to, char *buf, ssize_t n,
+		      const char *file_to, int fd_from)
 {
-	int fd_from, fd_to;
-	ssize_t r, w, off;
-	char buf[BUF_SIZE];
+	ssize_t off = 0, w;
 
-	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
+	while (off < n)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		w = write(fd_to, buf + off, n - off);
+		if (w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+				file_to);
+			close_fail(fd_from);
+			close_fail(fd_to);
+			exit(99);
+		}
+		off += w;
+	}
+}
+
+static void first_read_open_from(const char *src, int *pfd_from,
+				 char *buf, ssize_t *pr)
+{
+	*pfd_from = open(src, O_RDONLY);
+	if (*pfd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
 		exit(98);
 	}
 
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	*pr = read(*pfd_from, buf, BUF_SIZE);
+	if (*pr == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
+		close_fail(*pfd_from);
+		exit(98);
+	}
+}
+
+static void copy_rest(int fd_from, const char *src, const char *dst,
+		      char *buf, ssize_t r)
+{
+	int fd_to;
+
+	fd_to = open(dst, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		closing(fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dst);
+		close_fail(fd_from);
 		exit(99);
 	}
 
-	while ((r = read(fd_from, buf, BUF_SIZE)) > 0)
+	while (r > 0)
 	{
-		off = 0;
-		while (off < r)
+		write_all(fd_to, buf, r, dst, fd_from);
+		r = read(fd_from, buf, BUF_SIZE);
+		if (r == -1)
 		{
-			w = write(fd_to, buf + off, r - off);
-			if (w == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-				closing(fd_from);
-				closing(fd_to);
-				exit(99);
-			}
-			off += w;
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+				src);
+			close_fail(fd_from);
+			close_fail(fd_to);
+			exit(98);
 		}
 	}
-	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		closing(fd_from);
-		closing(fd_to);
-		exit(98);
-	}
 
-	closing(fd_from);
-	closing(fd_to);
-	return (0);
+	close_fail(fd_from);
+	close_fail(fd_to);
 }
 
 /**
- * main - entry point
- * @argc: args count
- * @argv: args vector
- * Return: 0 on success, exits with 97 on wrong usage
+ * main - copy content of a file to another file
+ * @ac: argument count
+ * @av: argument vector
+ * Return: 0 on success; exits 97/98/99/100 on error
  */
-int main(int argc, char *argv[])
+int main(int ac, char **av)
 {
-	if (argc != 3)
+	int fd_from;
+	ssize_t r;
+	char buf[BUF_SIZE];
+
+	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	return (copy_file(argv[1], argv[2]));
+
+	first_read_open_from(av[1], &fd_from, buf, &r);
+	copy_rest(fd_from, av[1], av[2], buf, r);
+	return (0);
 }
