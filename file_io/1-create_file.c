301fad978db3e4424dcd635931136d7f6d75663c
Shadenm-404 <viuxwl7@gmail.com>
2025-10-18 03:28:47 +0000

    file_io: update source files

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * create_file - creates a file and writes text_content into it
+ * create_file - creates a file and writes text
  * @filename: name of the file to create
- * @text_content: NULL terminated string to write to the file
+ * @text_content: string to write (NULL creates empty file)
  *
  * Return: 1 on success, -1 on failure
  */
@@ -15,7 +15,6 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	/* O_TRUNC يفرّغ الملف إن كان موجود، O_CREAT ينشئه إن لم يكن */
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
