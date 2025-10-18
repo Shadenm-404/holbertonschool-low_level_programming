6e4826de37c18392c8ba86b8280118686a5beb54
Shaden Alalwani <viuxwl7@gmail.com>
2025-10-18 14:06:51 +0300

    Update 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,52 +3,36 @@
 #include <unistd.h>     /* read, write, close */
 #include <fcntl.h>      /* open */
 #include <sys/stat.h>   /* modes */
+#include "main.h"       /* اختياري، موجود عندك */
 
 #define BUF_SIZE 1024
 
-static void exit_read(const char *file, int fd_from, int fd_to)
+/* ===== Helpers (≤ 5 functions إجمالاً) ===== */
+
+static void close_fail(int fd)
 {
-	dprintf(STDERR_FILENO,
-		"Error: Can't read from file %s\n", file);
-	if (fd_from >= 0 && close(fd_from) == -1)
+	if (close(fd) == -1)
 	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't close fd %d\n", fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
-	if (fd_to >= 0 && close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+}
+
+static void read_fail(const char *file, int fd_from, int fd_to, int to_opened)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+	close_fail(fd_from);
+	if (to_opened)
+		close_fail(fd_to);
 	exit(98);
 }
 
-static void exit_write(const char *file, int fd_from, int fd_to)
+static void write_fail(const char *file, int fd_from, int fd_to)
 {
-	dprintf(STDERR_FILENO,
-		"Error: Can't write to %s\n", file);
-	if (fd_from >= 0 && close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-	if (fd_to >= 0 && close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+	close_fail(fd_from);
+	close_fail(fd_to);
 	exit(99);
-}
-
-static void exit_close(int fd)
-{
-	dprintf(STDERR_FILENO,
-		"Error: Can't close fd %d\n", fd);
-	exit(100);
 }
 
 static void write_all(int fd_to, char *buf, ssize_t n,
@@ -60,7 +44,7 @@ static void write_all(int fd_to, char *buf, ssize_t n,
 	{
 		w = write(fd_to, buf + off, n - off);
 		if (w == -1)
-			exit_write(file_to, fd_from, fd_to);
+			write_fail(file_to, fd_from, fd_to);
 		off += w;
 	}
 }
@@ -73,48 +57,50 @@ static void write_all(int fd_to, char *buf, ssize_t n,
  */
 int main(int ac, char **av)
 {
-	int fd_from, fd_to, c1, c2;
+	int fd_from, fd_to;
 	ssize_t r;
 	char buf[BUF_SIZE];
 
 	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO,
-			"Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
+	/* افتح المصدر */
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-		exit_read(av[1], -1, -1);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
 
-	/* أول قراءة: إن فشلت نخرج 98 بدون فتح الوجهة */
+	/* أول read: إن فشلت نخرج 98 بدون فتح الوجهة */
 	r = read(fd_from, buf, BUF_SIZE);
 	if (r == -1)
-		exit_read(av[1], fd_from, -1);
+		read_fail(av[1], fd_from, -1, 0);
 
+	/* الآن فقط افتح الوجهة */
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		if (close(fd_from) == -1)
-			exit_close(fd_from);
-		exit_write(av[2], -1, -1);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		close_fail(fd_from);
+		exit(99);
 	}
 
-	/* اكتب الدفعة الأولى ثم أكمل بقية الدُفعات */
-	do {
+	/* اكتب الدفعة الأولى ثم أكمل بقيتها */
+	while (r > 0)
+	{
 		write_all(fd_to, buf, r, av[2], fd_from);
+
 		r = read(fd_from, buf, BUF_SIZE);
 		if (r == -1)
-			exit_read(av[1], fd_from, fd_to);
-	} while (r > 0);
+			read_fail(av[1], fd_from, fd_to, 1);
+	}
 
-	c1 = close(fd_from);
-	if (c1 == -1)
-		exit_close(fd_from);
-	c2 = close(fd_to);
-	if (c2 == -1)
-		exit_close(fd_to);
-
+	/* إغلاقات آمنة */
+	close_fail(fd_from);
+	close_fail(fd_to);
 	return (0);
 }
