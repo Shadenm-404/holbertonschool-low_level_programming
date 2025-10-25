8d7ffd7c4ebea85661280e178000ba28fb8c3b57
Shaden Alalwani <viuxwl7@gmail.com>
2025-10-25 14:18:42 +0000

    Add 1-djb2.c: djb2 hash function and prototype

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -5,30 +5,24 @@
 #include <string.h>
 #include <stdio.h>
 
-/**
- * struct hash_node_s - Node of a hash table
- * @key: The key
- * @value: The value corresponding to a key
- * @next: Pointer to the next node in the list
- */
+/* ... typedefs تبعك كما هي ... */
+
 typedef struct hash_node_s
 {
-	char *key;
-	char *value;
-	struct hash_node_s *next;
+    char *key;
+    char *value;
+    struct hash_node_s *next;
 } hash_node_t;
 
-/**
- * struct hash_table_s - Hash table data structure
- * @size: Size of the array
- * @array: Array of size @size
- */
 typedef struct hash_table_s
 {
-	unsigned long int size;
-	hash_node_t **array;
+    unsigned long int size;
+    hash_node_t **array;
 } hash_table_t;
 
+/* ===== Function prototypes ===== */
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+/* (لاحقاً ستضيفين هنا key_index وباقي الدوال) */
 
 #endif /* HASH_TABLES_H */
